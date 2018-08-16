492b7db4c297bac9cd42e060f24b43711d6566c3
Jose Luis Eiguren <joseluiseiguren@gmail.com>
2018-08-16 17:54:01 +0200

    first

---
+++ EntityFrameworkCore/Program.cs
@@ -0,0 +1,21 @@
+﻿using entFrExample.Models;
+using System;
+using System.Linq;
+
+namespace entFrExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var dbctx = new DipositsContext())
+            {
+                var x = dbctx.Address.Where(p => p.Idaddress == 10010).FirstOrDefault();
+
+                var y = dbctx.Address.Where(p => p.Idaddress == 100100).FirstOrDefault();
+            }
+        }
+    }
+}
